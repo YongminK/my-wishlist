3afe20ca20b0e2c81aa3e01d4b0e56d461269b5f
e.petrova <e.petrova@int.robotbull.com>
2021-11-21 12:22:14 +0300

    removed ttf-loader

--- next.config.js
+++ next.config.js
@@ -4,20 +4,4 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  module: {
-    rules: [
-      {
-        test: /\.ttf$/,
-        use: [
-          {
-            loader: 'ttf-loader',
-            options: {
-              name: 'fonts/[hash].[ext]',
-            },
-          },
-        ]
-      }
-    ]
-  },
-  watch: true
 }
